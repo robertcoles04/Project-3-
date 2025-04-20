27662ab48a0fbd69cad636571f39f861f55a7941
romanferraraufl <roman.ferrara@ufl.edu>
2025-04-20 14:33:00 -0400

    Moved the parsing of the dataset to the beginning of main. Added a loop so the user can choose to go again. NOTE: I did not write the code for the parsing of the dataset! Robert wrote that so he should get the credit, I just moved it to the beginning of main.

--- src/main.cpp
+++ src/main.cpp
@@ -11,46 +11,14 @@
 using namespace std;
 
 int main() {
-    string title;
-    string artist;
-    string album;
-    string genre;
-    string releaseDate;
-    int duration;
-    int popularity;
-
     ifstream file("music.csv");
     if (!file.is_open()) {
         cerr << "Failed to open music.csv" << endl;
         return 1;
     }
-
-    cout << "What release year would you like the song to be? " << endl;
-    cin >> releaseDate;
-
-    cout << "Would you like a short or long duration? (short/long) " << endl;
-    string durPref;
-    cin >> durPref;
-    if (durPref == "short" || durPref == "Short") {
-        duration = 180;
-    } else if (durPref == "long" || durPref == "Long") {
-        duration = 300;
-    } else {
-        duration = 240;
-    }
-
-    cout << "How popular do you want the song to be? (0 - 100) " << endl;
-    cin >> popularity;
-
-    cout << "\nYou chose:\n"
-         << "Year:" << releaseDate << "\n"
-         << "Max Length: " << duration << " seconds\n"
-         << "Popularity: " << popularity << " (0-100)\n";
-
-    vector<Song> filteredSongs;
+    vector<Song> songs;
     string line;
     getline(file, line);
-
     while (getline(file, line)) {
         stringstream ss(line);
         string t, a, al, g, rd, dur, pop;
@@ -64,62 +32,91 @@ int main() {
         getline(ss, pop, ',');
 
         Song song(t, a, al, g, rd, dur, pop);
-
-        if (song.getReleaseDate().substr(0, 4) == releaseDate &&
-            song.getDuration() <= duration &&
-            song.getPopularity() >= popularity) {
-            filteredSongs.push_back(song);
-        }
+        songs.push_back(song);
     }
-
     file.close();
 
-    if (filteredSongs.empty()) {
-        cout << "No songs matched your filter criteria.\n";
-        return 0;
+    while (true) {
+        string releaseDate;
+        int duration;
+        int popularity;
+
+        cout << "What release year would you like the song to be? " << endl;
+        cin >> releaseDate;
+
+        cout << "Would you like a short or long duration? (short/long) " << endl;
+        string durPref;
+        cin >> durPref;
+        if (durPref == "short" || durPref == "Short") {
+            duration = 180;
+        } else if (durPref == "long" || durPref == "Long") {
+            duration = 300;
+        } else {
+            duration = 240;
+        }
+
+        cout << "How popular do you want the song to be? (0 - 100) " << endl;
+        cin >> popularity;
+
+        cout << "\nYou chose:\n"
+             << "Year:" << releaseDate << "\n"
+             << "Max Length: " << duration << " seconds\n"
+             << "Popularity: " << popularity << " (0-100)\n";
+
+        cout << "\nSort by which attribute? (duration / popularity / release date): ";
+        string sortBy;
+        cin >> sortBy;
+
+        vector<Song> mergeSongs = songs;
+        vector<Song> quickSongs = songs;
+
+        auto startMerge = chrono::high_resolution_clock::now();
+        mergeSort(mergeSongs, 0, mergeSongs.size() - 1, sortBy);
+        auto endMerge = chrono::high_resolution_clock::now();
+        auto mergeTime = chrono::duration_cast<chrono::milliseconds>(endMerge - startMerge).count();
+
+        auto startQuick = chrono::high_resolution_clock::now();
+        quickSort(quickSongs, 0, quickSongs.size() - 1, sortBy);
+        auto endQuick = chrono::high_resolution_clock::now();
+        auto quickTime = chrono::duration_cast<chrono::milliseconds>(endQuick - startQuick).count();
+
+        /* uncomment once binarySearch is done
+        vector<Song> filteredSongs = binarySearch(songs, 0, songs.size()-1, "duration", 50);
+        if (filteredSongs.empty()) {
+            cout << "No songs matched your filter criteria.\n";
+            return 0;
+        }*/
+
+        cout << "\nTop 10 songs sorted by " << sortBy << " using Merge Sort:\n";
+        for (int i = 0; i < min(10, (int)mergeSongs.size()); ++i) {
+            const Song& s = mergeSongs[i];
+            cout << s.getTitle() << " | " << s.getArtist()
+                 << " | " << s.getGenre()
+                 << " | " << s.getReleaseDate()
+                 << " | " << s.getDuration()
+                 << "s | " << s.getPopularity() << endl;
+        }
+
+        cout << "\nTop 10 songs sorted by " << sortBy << " using Quick Sort:\n";
+        for (int i = 0; i < min(10, (int)quickSongs.size()); ++i) {
+            const Song& s = quickSongs[i];
+            cout << s.getTitle() << " | " << s.getArtist()
+                 << " | " << s.getGenre()
+                 << " | " << s.getReleaseDate()
+                 << " | " << s.getDuration()
+                 << "s | " << s.getPopularity() << endl;
+        }
+
+        cout << "\nSorting Time Comparison:\n";
+        cout << "Merge Sort: " << mergeTime << " ms\n";
+        cout << "Quick Sort: " << quickTime << " ms\n";
+        string goAgain;
+        cout << "Would like to go again? Y or y for yes, any other key for no" << endl;
+        cin >> goAgain;
+        if (goAgain != "Y" && goAgain != "y") {
+            break;
+        }
     }
-
-    cout << "\nSort by which attribute? (duration / popularity / release date): ";
-    string sortBy;
-    cin >> sortBy;
-
-    vector<Song> mergeSongs = filteredSongs;
-    vector<Song> quickSongs = filteredSongs;
-
-
-    auto startMerge = chrono::high_resolution_clock::now();
-    mergeSort(mergeSongs, 0, mergeSongs.size() - 1, sortBy);
-    auto endMerge = chrono::high_resolution_clock::now();
-    auto mergeTime = chrono::duration_cast<chrono::milliseconds>(endMerge - startMerge).count();
-
-    auto startQuick = chrono::high_resolution_clock::now();
-    quickSort(quickSongs, 0, quickSongs.size() - 1, sortBy);
-    auto endQuick = chrono::high_resolution_clock::now();
-    auto quickTime = chrono::duration_cast<chrono::milliseconds>(endQuick - startQuick).count();
-
-    cout << "\nTop 10 songs sorted by " << sortBy << " using Merge Sort:\n";
-    for (int i = 0; i < min(10, (int)mergeSongs.size()); ++i) {
-        const Song& s = mergeSongs[i];
-        cout << s.getTitle() << " | " << s.getArtist()
-             << " | " << s.getGenre()
-             << " | " << s.getReleaseDate()
-             << " | " << s.getDuration()
-             << "s | " << s.getPopularity() << endl;
-    }
-
-    cout << "\nTop 10 songs sorted by " << sortBy << " using Quick Sort:\n";
-    for (int i = 0; i < min(10, (int)quickSongs.size()); ++i) {
-        const Song& s = quickSongs[i];
-        cout << s.getTitle() << " | " << s.getArtist()
-             << " | " << s.getGenre()
-             << " | " << s.getReleaseDate()
-             << " | " << s.getDuration()
-             << "s | " << s.getPopularity() << endl;
-    }
-
-    cout << "\nSorting Time Comparison:\n";
-    cout << "Merge Sort: " << mergeTime << " ms\n";
-    cout << "Quick Sort: " << quickTime << " ms\n";
 
     return 0;
 }

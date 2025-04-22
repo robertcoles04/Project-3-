cf1a0319bfd1af06a9f5a0bf5e2585ebbb2ecd52
Patrick Cooney <148221095+PatCooney06@users.noreply.github.com>
2025-04-21 23:03:32 -0400

    Updated binary search

--- src/binarySearch.h
+++ src/binarySearch.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Song.h"
 #include <vector>
+#include <type_traits>
 
 using namespace std;
 
@@ -11,9 +12,9 @@ vector<Song> binaryGenre(std::vector<Song> songs, string target, int low, int hi
         int mid = (high - low) / 2 + low;
 
         if (target == songs[mid].getGenre()) {
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < 100; i++) {
                 //Add the 10 songs surrounding mid
-                result.push_back(songs[mid - 5 + i]);
+                result.push_back(songs[mid - 50 + i]);
             }
             return result;
         }
@@ -35,9 +36,9 @@ vector<Song> binaryRD(std::vector<Song> songs, string target, int low, int high)
         int mid = (high - low) / 2 + low;
 
         if (target == songs[mid].getReleaseDate()) {
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < 5000; i++) {
                 //Add the 10 songs surrounding mid
-                result.push_back(songs[mid - 5 + i]);
+                result.push_back(songs[mid - 2500 + i]);
             }
             return result;
         }
@@ -53,15 +54,15 @@ vector<Song> binaryRD(std::vector<Song> songs, string target, int low, int high)
 }
 
 // For Duration
-int binaryDuration(std::vector<Song> songs, int target, int low, int high) {
+vector<Song> binaryDuration(std::vector<Song> songs, int target, int low, int high) {
     vector<Song> result;
     if (high >= low) {
         int mid = (high - low) / 2 + low;
 
         if (target == songs[mid].getDuration()) {
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < 5000; i++) {
                 //Add the 10 songs surrounding mid
-                result.push_back(songs[mid - 5 + i]);
+                result.push_back(songs[mid - 2500 + i]);
             }
             return result;
         }
@@ -83,9 +84,9 @@ vector<Song> binaryPop(std::vector<Song> songs, int target, int low, int high) {
         int mid = (high - low) / 2 + low;
 
         if (target == songs[mid].getPopularity()) {
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < 5000; i++) {
                 //Add the 10 songs surrounding mid
-                result.push_back(songs[mid - 5 + i]);
+                result.push_back(songs[mid - 2500 + i]);
             }
             return result;
         }
@@ -102,30 +103,56 @@ vector<Song> binaryPop(std::vector<Song> songs, int target, int low, int high) {
 
 // CALL THIS. Input vector of song objects, 0 for low index, songs.size() - 1, attribute of song, and search item
 vector<Song> binarySearch(std::vector<Song>& songs, int low, int high, const string& type, auto target) {
-    //NEED TO RETURN VECTOR
-
     //Search based on attribute
     if (type == "genre") {
+        string target1;
+        if constexpr (std::is_same<decltype(target), std::string>::value) {
+            target1 = target;
+        }
+        else {
+            target1 = std::to_string(target);
+        }
         vector<Song> genreSongs;
-        genreSongs = binaryGenre(songs, target, low, high);
+        genreSongs = binaryGenre(songs, target1, low, high);
         return genreSongs;
     }
 
-    if (type == "release date") {
+    if (type == "releaseDate") {
+        string target1;
+        if constexpr (std::is_same<decltype(target), std::string>::value) {
+            target1 = target;
+        }
+        else {
+            target1 = std::to_string(target);
+        }
         vector<Song> releaseSongs;
-        binaryRD(songs, target, low, high);
+        releaseSongs = binaryRD(songs, target1, low, high);
         return releaseSongs;
     }
 
     if (type == "duration") {
+        int target1;
+        if constexpr (std::is_same<decltype(target), int>::value) {
+            target1 = target;
+        }
+        else {
+            target1 = stoi(target);
+        }
         vector<Song> durationSongs;
-        binaryDuration(songs, target, low, high);
+        durationSongs = binaryDuration(songs, target1, low, high);
         return durationSongs;
     }
 
     if (type == "popularity") {
+        int target1;
+        if constexpr (std::is_same<decltype(target), int>::value) {
+            target1 = target;
+        }
+        else {
+            target1 = stoi(target);
+        }
         vector<Song> popularitySongs;
-        popularitySongs = binaryPop(songs, target, low, high);
+        popularitySongs = binaryPop(songs, target1, low, high);
         return popularitySongs;
     }
 }
